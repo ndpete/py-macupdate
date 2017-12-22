4fb0624c65f4cc6167a876b7d2f2683266ef8799
Nathan Peterson <ndpete@gmail.com>
2017-12-22 15:01:31 -0700

    initial system updates implemented

--- py_macupdate/system_cmd/commands.py
+++ py_macupdate/system_cmd/commands.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3.6
 
 import click
+import subprocess
 
 
 @click.command('update')
-def update():
+@click.option('-f', '--force', is_flag=True, help="Supress Confirmation")
+def update(force):
     """Run OS Updates"""
-    click.secho("Not implemented yet", fg='red')
+    if not force:
+        click.confirm("Warning, this will install all system updates and may require a restart, Continue",
+                      default=True, show_default=True, abort=True)
+    cmd = "sudo -k softwareupdate -i -a"
+    click.secho("Prompting for sudo password", fg='blue')
+    subprocess.run(cmd.split(" "))
