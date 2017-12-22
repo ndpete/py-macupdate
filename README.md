0bc01c009ae63b8a413ec1c4d87eaea2fbd33c10
Nathan Peterson <ndpete@gmail.com>
2017-12-22 13:55:27 -0700

    initial features for pipsi implemented

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # py-macupdate
 Simple CLI suite to run updates for my mac
+
+This is very alpha and not alot of effor has gone in to detecting potential errors and handling them.
--- py_macupdate/pipsi/commands.py
+++ py_macupdate/pipsi/commands.py
@@ -1,15 +1,67 @@
 #!/usr/bin/env python3.6
-
 import click
+import os
+import subprocess
 
 
 @click.command('update')
-def pipsi_update():
+@click.option('--pkg', type=click.STRING, help='Update single package')
+def pipsi_update(pkg):
     """Update pipsi command environments"""
-    click.secho("Not implemented yet", fg='red')
+    commands = []
+    if pkg:
+        cmd = ['pipsi', 'upgrade', pkg]
+        commands.append(cmd)
+    else:
+        packages = get_installed()
+        if packages:
+            for pkg in packages:
+                cmd = ['pipsi', 'upgrade', pkg]
+                commands.append(cmd)
+
+    for cmd in commands:
+        click.secho(f'Upgrading: {cmd[2]}', fg='blue')
+        run_cmd(cmd)
 
 
 @click.command('upgrade-py')
-def pipsi_upgrade():
+@click.option('--pkg', type=click.STRING, help='Update single package')
+@click.option('-f', '--force', is_flag=True, help="Supress Confirmation")
+def pipsi_upgrade(pkg, force):
     """Upgrade Python Env for pipsi commands"""
-    click.secho("Not implemented yet", fg='red')
+    if not force:
+        click.confirm("Warning, this will uninstall and reinstall pipsi installed scripts, Continue",
+                      default=True, show_default=True, abort=True)
+    commands = []
+    if pkg:
+        rmcmd = ['pipsi', 'uninstall', '--yes', pkg]
+        newcmd = ['pipsi', 'install', pkg]
+        commands.append(rmcmd)
+        commands.append(newcmd)
+    else:
+        packages = get_installed()
+        if packages:
+            for pkg in packages:
+                rmcmd = ['pipsi', 'uninstall', '--yes', pkg]
+                newcmd = ['pipsi', 'install', pkg]
+                commands.append(rmcmd)
+                commands.append(newcmd)
+
+    for cmd in commands:
+        if cmd[2] == '--yes':
+            click.secho(f'Upgrading: {cmd[3]}', fg='blue')
+        run_cmd(cmd)
+
+
+def get_installed():
+    path = os.path.join(os.path.expanduser('~'), '.local', 'venvs')
+
+    folders = []
+    if os.path.isdir(path):
+        folders = [f for f in os.listdir(
+            path) if os.path.isdir(os.path.join(path, f))]
+    return folders
+
+
+def run_cmd(cmd: list):
+    return subprocess.run(cmd)
